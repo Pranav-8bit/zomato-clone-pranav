a120c3b7c2f050e83f44e7923bde3f9cdfdb9e3f
Pranav-8bit <tpranav15@gmail.com>
2022-10-23 16:34:46 +0530

    completed setup of server, added bable to it and made mongo connection.

---
+++ Server/database/allModels.js
@@ -0,0 +1,17 @@
+import { FoodModel } from "./food";
+import { ImageModel } from "./image";
+import { MenuModel } from "./menu";
+import { OrderModel } from "./order";
+import { RestaurantModel } from "./restaurant";
+import { ReviewModel } from "./review";
+import { UserModel } from "./user";
+
+export {
+  ImageModel,
+  MenuModel,
+  OrderModel,
+  FoodModel,
+  RestaurantModel,
+  ReviewModel,
+  UserModel,
+};

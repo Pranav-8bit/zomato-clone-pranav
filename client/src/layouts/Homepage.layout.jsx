cff03e620841922151aa7f0142b2733103ae3b14
Pranav-8bit <tpranav15@gmail.com>
2022-11-04 23:58:28 +0530

    Completed home page navbar and Completed mobile food tab for home page

--- client/src/layouts/Homepage.layout.jsx
+++ client/src/layouts/Homepage.layout.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-// import Nvabar from "../components/Navbar/";
-// import FoodTab from "../components/FoodTab";
+import React from "react";
+import Navbar from "../components/Navbar";
+import FoodTab from "../components/FoodTab";
+
 const HomepageLayout =
-  (Components) =>
+  (Component) =>
   ({ ...props }) => {
     return (
       <>
-        {/* <Navbar />
-        <FoodTab /> */}
+        <Navbar />
+        <FoodTab />
         <div className="container mx-auto px-4 lg:px-20">
           <Component {...props} />
         </div>
--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-//Layout
+// Layout
 import HomeLayout from "../layouts/Homepage.layout";
 
 const Home = () => {
